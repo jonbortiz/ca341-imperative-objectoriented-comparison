e9ad94ed71bdf0b1ca844e7f10765f21764fa2dd
Natasha Mccarthy <natashamccarthy@Natashas-MacBook-Pro.local>
2022-10-26 12:14:18 +0100

    completed, with print and delete

--- oo_implementation/oo_phonebook.py
+++ oo_implementation/oo_phonebook.py
@@ -1,35 +1,65 @@
 #!/usr/bin/env python3
 
-# Program is not fully complete: 
+# Program to be completed 
 
-class Node:                                         # Create node class so we can assign our name, address and number to the class to add to tree
+class Node:                                         # Create node class so we can assign our name, address and number to add to tree
     def __init__(self, name, address, number):
         self.data = number
         self.name = name
         self.address = address
-        self.left = None                            # assigning left and right node to none so we can insert to tree later
+        self.left = None                                                # assigning left and right node to none so we can insert to tree also
         self.right = None 
 
-def insert(root, name, address, number):               # function to insert node to binary tree sorted by phone number
+def insert(root, name, address, number):                                # function to insert node to binary tree sorted by phone number
     if root is None:                            
-        return Node(name, address, number)             # if root is empty will assign node to binary tree
+        return Node(name, address, number)                              # if root is empty will assign node to binary tree
     else: 
         if root.data == number:
             return root
-        elif root.data < number:                       # checks if node inserted can go in the left or right leaf depending on how number is sorted
-            root.right = insert(root.right, name, address, number)      # using recursion to call again with certain direction
+        elif root.data < number:                                        # checks if node inserted can go in the left or right leaf depending on how number is sorted
+            root.right = insert(root.right, name, address, number)      # uses recursion to find where node is placed in binary tree
         else:
             root.left = insert(root.left, name, address, number)
     return root
 
-def search_contact(root, number):                                       # function to search for contact on binary tree
+def search_contact(root, number):                                       # function to search for contact on binary tree based on number
     if root is None or root.data == number: 
         return root.name, root.address                                 # returns the name and address linked with phone number given in function 
     
     if root.data < number:                                             # if not found yet, function is called again to look at left or right node depending on how it is sorted
         return search_contact(root.right, number)
     return search_contact(root.left, number)
+
+def delete_contact(root, number):                                      # function to delete contact
+    if root is None:
+        return root
+
+    if root.data > number:
+        root.left = delete_contact(root.left, number)
+    elif root.data < number:
+        root.right = delete_contact(root.right, number)
+    else: 
+        if root.right is None:
+            return root.left
         
+        if root.left is None:
+            return root.right
+
+        temp = root.right
+        minimum_node = temp.data
+
+        while temp.left:
+            temp = temp.left
+            minimum_node = temp.data
+        root.right = delete_contact(root.right, number)
+    return root
+        
+def print_contacts_inorder(root):
+    if root:
+        print_contacts_inorder(root.left)
+        print(root.data, root.name, root.address)
+        print_contacts_inorder(root.right)
+
 def read_file():                                                        # using function to read our file which has all of our contacts from phonebook stored
     contacts = {}
     with open('details.txt') as f:
@@ -51,7 +81,7 @@ def main():
 
     print("\nWelcome to the phonebook command line")
     
-    print("Commands in use: search, new, delete, quit")
+    print("Commands in use: search, new, delete, quit, print")
     print("____________________________________________________________________________\n")
 
     while True:                                                         # command line is running while statement is true, will only exit program once user types "exit"
@@ -67,15 +97,24 @@ def main():
             except AttributeError:
                 print("{} not found in phonebook".format(number))
         
+        if(command == "print"):
+            print_contacts_inorder(root)
+        
         elif(command == "new"):
             name = input("Enter Name: ")
             address = input("Enter County: ")
             number = int(input("Enter Number: "))
             insert(root, name, address, number)
             phonebook[name] = (address, number)
-            file.write("\n{}, {}, {}".format(name, address, number))
+            file.write("\n{} {} {}".format(name, address, number))
             file.close()
             print("Contact added to phonebook!")
+        
+        elif(command == "delete"):
+            number = int(input("Enter Number: "))
+            delete_contact(root, number)
+            print_contacts_inorder(root)
+            print("Contact deleted!")
 
 if __name__ == "__main__":
     main()
